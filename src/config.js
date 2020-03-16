b2190809e21aeeb6f083479994ebef82b8736e8d
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-16 13:57:49 -0600

    Initial backend setup

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL
-    || 'postgresql://germin@localhost/sparep-test',
+    || 'postgresql://germin:Nhgi-Will@localhost/sparep-test',
   JWT_SECRET: process.env.JWT_SECRET || 'abcITSeasyAS123',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
