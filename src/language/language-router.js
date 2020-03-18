f62b06f87e927ffce9ab1c3b365cbc0dc1944962
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-18 16:16:12 -0600

    Fixed Linked List

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const LanguageService = require("./language-service");
 const { requireAuth } = require("../middleware/jwt-auth");
 const jsonBodyParser = express.json();
+const LinkedList  =require('./LinkedList');
 
 const languageRouter = express.Router();
 
@@ -40,7 +41,7 @@ languageRouter.get("/", async (req, res, next) => {
   }
 });
 
-languageRouter.get("/head", async (req, res, next) => {
+languageRouter.get("/head", jsonBodyParser, async (req, res, next) => {
   try {
     const head = await LanguageService.getLanguageHead(
       req.app.get("db"),
@@ -52,7 +53,8 @@ languageRouter.get("/head", async (req, res, next) => {
       totalScore: req.language.total_score,
       wordCorrectCount: head.correct_count,
       wordIncorrectCount: head.incorrect_count,
-      answer : head.translation
+      answer : head.translation,
+      nextWord : head.next
     });
     next();
   } catch (error) {
@@ -60,92 +62,104 @@ languageRouter.get("/head", async (req, res, next) => {
   }
 });
 
-languageRouter.post("/guess", jsonBodyParser, async (req, res, next) => {
-  try {
-    const { guess } = req.body;
 
-    if (!req.body.guess) {
-      return res.status(400).json({ error: `You did not make a guess.` });
+languageRouter.post('/guess', jsonBodyParser, async (req, res, next) => {
+    try {
+        const { guess } = req.body;
+        
+        if(!guess) {
+            return res.status(400).json( {error: `Missing 'guess' in request body`} );
+        }
+        
+        
+        const DynamicWordList = new LinkedList();
+        const db = req.app.get('db');
+        
+        //From language find and insert the head first into linked list DynamicWordList.
+        let DynamicWordListHead = await LanguageService.getLanguageHead(db, req.language.head);
+        console.log(DynamicWordList);
+        console.log(DynamicWordListHead);
+        let words = await LanguageService.getLanguageWords(db, req.language.id);
+        DynamicWordList.insertFirst(DynamicWordListHead[0]);
+        
+        
+        // Need to populate the rest of the linked DynamicWordList according to the "next" values
+        while(DynamicWordListHead[0].next !== null) {
+            let currNode = words.find(word => word.id === DynamicWordListHead[0].next);
+            DynamicWordList.insertLast(currNode);
+            DynamicWordListHead = [currNode];
+        }
+    
+        console.log(DynamicWordList);
+        console.log(DynamicWordListHead);
+        let isCorrect;
+        
+        if(DynamicWordList.head.value.translation.toLowerCase() === guess.toLowerCase()) {
+            isCorrect = true;
+            DynamicWordList.head.value.memory_value *= 2;
+            DynamicWordList.head.value.correct_count++;
+            req.language.total_score++;
+        } else {
+            isCorrect = false;
+            DynamicWordList.head.value.memory_value = 1;
+            DynamicWordList.head.value.incorrect_count++;
+        }
+    
+        console.log(DynamicWordList);
+        console.log(DynamicWordListHead);
+        //Store value of head then remove and dynamically insert based on memory_value
+        const removedHead = DynamicWordList.head.value;
+        DynamicWordList.remove(DynamicWordList.head.value);
+        DynamicWordList.insertAt(removedHead, removedHead.memory_value);
+    
+    
+        console.log(DynamicWordList);
+        console.log(DynamicWordListHead);
+        //Now that we removed the previous head, we are storing the value of the new head
+        let tempNode = DynamicWordList.head;
+        let newHead = tempNode.value.id;
+    
+        console.log(DynamicWordList);
+        console.log(DynamicWordListHead);
+        //The following loop keeps the original order of the data by updating each
+        while(tempNode !== null) {
+            await LanguageService.updateWord(
+                db,
+                tempNode.value.id,
+                {
+                    memory_value: tempNode.value.memory_value,
+                    correct_count: tempNode.value.correct_count,
+                    incorrect_count: tempNode.value.incorrect_count,
+                    next: tempNode.next !== null ? tempNode.next.value.id : null
+                }
+            );
+            tempNode = tempNode.next;
+        }
+        
+        //Updating the Language table so that the new HEAD is reflected and total score is updated
+        await LanguageService.updateLanguage(
+            db,
+            req.user.id,
+            {
+                total_score: req.language.total_score,
+                head : newHead
+            }
+        );
+        console.log(DynamicWordList.head.value.original);
+        const response = {
+            nextWord: DynamicWordList.head.value.original,
+            wordCorrectCount: DynamicWordList.head.value.correct_count,
+            wordIncorrectCount: DynamicWordList.head.value.incorrect_count,
+            totalScore: req.language.total_score,
+            answer: removedHead.translation,
+            isCorrect
+        };
+        
+        
+        return res.status(200).json(response);
+    } catch(error) {
+        next(error);
     }
-
-    const words = await LanguageService.getLanguageWords(
-      req.app.get("db"),
-      req.language.id
-    );
-
-    /**
-      If the answer was correct:
-      Double the memory value
-      Else, if the answer was wrong:
-      Reset memory value to 1
-     */
-    if (guess.toLowerCase() === words[0].translation.toLowerCase()) {
-      words[0].correct_count++;
-      words[0].memory_value *= 2;
-      req.language.total_score++;
-    } else {
-      words[0].incorrect_count++;
-      words[0].memory_value = 1;
-    }
-
-    await LanguageService.updateTotalScore(
-      req.app.get("db"),
-      req.language.id,
-      req.language.total_score
-    );
-
-    let LL = LanguageService.createLinkedList(req.language.head, words);
-    const listSize = LL.size();
-
-    const currentWord = LL.head.value;
-
-    if (LL.head.value.memory_value >= listSize) {
-      LL.remove(words[0]);
-      LL.insertLast(words[0]);
-    } else {
-      LL.remove(words[0]);
-      LL.insertAt(words[0].memory_value, words[0]);
-    }
-
-    const sortedList = LL.displayList();
-
-    for (let i = 0; i < sortedList.length; i++) {
-      if (sortedList[i + 1]) {
-        sortedList[i].next = sortedList[i + 1].id;
-      } else {
-        sortedList[i].next = null;
-      }
-      await LanguageService.updateWord(
-        req.app.get("db"),
-        sortedList[i].id,
-        sortedList[i]
-      );
-    }
-
-    /**
-       *
-    "nextWord": "test-next-word-from-incorrect-guess",
-    "wordCorrectCount": 888,
-    "wordIncorrectCount": 111,
-    "totalScore": 999,
-    "answer": "test-answer-from-incorrect-guess",
-    "isCorrect": false
-       */
-
-    const results = {
-      currentWord: currentWord.original,
-      nextWord: sortedList[0].original,
-      wordCorrectCount: sortedList[0].correct_count,
-      wordIncorrectCount: sortedList[0].incorrect_count,
-      totalScore: req.language.total_score,
-      answer: words[0].translation,
-      isCorrect: guess.toLowerCase() === words[0].translation.toLowerCase(),
-      guess: guess
-    };
-    return res.status(200).json(results);
-  } catch (error) {
-    next(error);
-  }
 });
 
 module.exports = languageRouter;
