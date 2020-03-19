11317f44f9ae1bf2878f1faf2a8182b6291ac482
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-19 11:21:34 -0600

    fixes lang router

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/spacer",
+  DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/spaced",
   JWT_SECRET: process.env.JWT_SECRET || "abcITSeasyAS123",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "3h"
 };
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,51 +1,88 @@
+const LinkedList = require('./LinkedList');
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
-        .from('language')
+      .from("language")
         .select(
-            'language.id',
-            'language.name',
-            'language.user_id',
-            'language.head',
-            'language.total_score',
+        "language.id",
+        "language.name",
+        "language.user_id",
+        "language.head",
+        "language.total_score"
         )
-        .where('language.user_id', user_id)
+      .where("language.user_id", user_id)
         .first();
   },
   
   getLanguageWords(db, language_id) {
     return db
-        .from('word')
+      .from("word")
         .select(
-            'id',
-            'language_id',
-            'original',
-            'translation',
-            'next',
-            'memory_value',
-            'correct_count',
-            'incorrect_count',
+        "word.id",
+        "word.language_id",
+        "word.original",
+        "translation",
+        "next",
+        "memory_value",
+        "correct_count",
+        "incorrect_count"
         )
-        .where({ language_id });
+      .where({ language_id });
   },
   
-  getLanguageHead(db, headId) {
+  getLanguageHead(db, language_id) {
     return db
-        .from('word')
-        .select('*')
-        .where('id', headId);
+      .from("word")
+        .select(
+        "id",
+        "language_id",
+        "original",
+        "translation",
+        "next",
+        "memory_value",
+        "correct_count",
+        "incorrect_count"
+        )
+      .where({ language_id })
+      .first();
+  },
+
+
+  
+  convertLinkedList(words) {
+    const linkedList = new LinkedList();
+    words.forEach(word => linkedList.insertLast(word));
+    return linkedList;
+  },
+
+
+  updateWord(db, id, data) {
+    return db
+      .from("word")
+      .where({ id })
+      .update({ ...data });
   },
   
-  updateLanguage(db, user_id, newLanguageFields) {
-    return db('language')
-        .where( {user_id} )
-        .update(newLanguageFields);
+  updateTotalScore(db, user_id) {
+    // Get current score and increment 1
+    const total_score =
+      db
+        .from("language")
+        .select("total_score")
+        .where({ user_id }) + 1;
+
+    return db
+      .from("language")
+      .where({ user_id })
+      .update({ total_score });
   },
   
-  updateWord(db, word_id, newWordFields) {
-    return db('word')
-        .where('id', word_id)
-        .update(newWordFields);
+  getAnswer(db, original) {
+    return db
+      .from("word")
+      .select("translation")
+      .where({ original });
   }
 };
 
